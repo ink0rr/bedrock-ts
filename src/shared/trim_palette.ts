6e3ad54d5fa5c4cfea035e0af7892e3bc3b4ee62
Svdex <renixiel@gmail.com>
2024-08-10 11:16:12 +0700

    Add trim palette type

---
+++ src/shared/trim_palette.ts
@@ -0,0 +1,12 @@
+export type TrimPalette<T extends string> = Partial<{
+  amethyst_palette: T;
+  copper_palette: T;
+  diamond_palette: T;
+  emerald_palette: T;
+  gold_palette: T;
+  iron_palette: T;
+  lapis_palette: T;
+  quartz_palette: T;
+  netherite_palette: T;
+  redstone_palette: T;
+}>;
