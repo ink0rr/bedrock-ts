e9a3fb7a886afa3be6a7c3927b59781579e2db17
Svdex <renixiel@gmail.com>
2024-08-10 11:16:02 +0700

    Split slot literals

--- src/shared/slot.ts
+++ src/shared/slot.ts
@@ -1,18 +1,22 @@
 import { LiteralUnion } from "./literal_union.js";
 
-export type Slot = LiteralUnion<
+export type WearableSlot = LiteralUnion<
   | "slot.armor"
   | "slot.armor.body"
   | "slot.armor.chest"
   | "slot.armor.feet"
   | "slot.armor.head"
   | "slot.armor.legs"
+  | "slot.weapon.offhand"
+>;
+
+export type Slot = LiteralUnion<
   | "slot.chest"
   | "slot.enderchest"
   | "slot.hotbar"
   | "slot.inventory"
   | "slot.saddle"
   | "slot.weapon.mainhand"
-  | "slot.weapon.offhand"
   | "slot.equippable"
+  | WearableSlot
 >;
