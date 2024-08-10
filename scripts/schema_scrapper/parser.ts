14c127fe1bb1b40fd6f2661e273977dbb0d19448
Svdex <renixiel@gmail.com>
2024-08-10 14:04:33 +0700

    Add glob scan versions

--- scripts/schema_scrapper/parser.ts
+++ scripts/schema_scrapper/parser.ts
@@ -1,10 +1,18 @@
+import { semver } from "bun";
 import path from "node:path/posix";
 import { readJson, writeFile } from "../util/fs";
 import { info } from "../util/log";
 import { parseSchema, SchemaProperty, snakeToPascal } from "../util/schema_parser";
 
+const versionRegex = /v(\d+\.\d+\.\d+)/;
+
 type ParseSchemasOptions = {
   excludeFiles?: string[];
+};
+
+type FileData = {
+  version: string;
+  filepath: string;
 };
 
 /**
@@ -14,8 +22,10 @@ type ParseSchemasOptions = {
  * @returns - Map of filename to typeName
  */
 export async function parseComponentSchemas(type: string, options: ParseSchemasOptions = {}) {
-  const glob = `./temp/editor-packages-main/packages/minecraftBedrock/schema/${type}/v*/**/*.json`;
+  const root = `./temp/editor-packages-main/packages/minecraftBedrock/schema/${type}`;
+  const pattern = `${root}/v*/**/*.json`;
   const scanned = new Map<string, string>();
+  const fileVersions = new Map<string, FileData>();
 
   let excludeFiles = options.excludeFiles || [
     "event",
@@ -25,9 +35,28 @@ export async function parseComponentSchemas(type: string, options: ParseSchemasO
     "blockTraits",
   ];
 
-  const entries = new Bun.Glob(glob).scan();
+  const entries = new Bun.Glob(pattern).scan();
   for await (const _f of entries) {
     const filepath = _f.replace(/\\/g, "/");
+    const filename = path.basename(filepath, ".json");
+
+    if (excludeFiles.includes(filename)) continue;
+
+    const scannedVersion = fileVersions.get(filename)?.version;
+    const version = versionRegex.exec(filepath)?.[1];
+
+    if (!version) continue;
+
+    if (!scannedVersion || semver.order(version, scannedVersion) > 0) {
+      fileVersions.set(filename, { version, filepath });
+    }
+  }
+
+  const fileEntries = Array.from(fileVersions.entries()).map(([filename, data]) => {
+    const relative = path.relative(root, data.filepath).split("/").slice(1).join("/");
+    return `${root}/v${data.version}/${relative}`;
+  });
+  for await (const filepath of fileEntries) {
     const filename = path.basename(filepath, ".json");
     if (excludeFiles.includes(filename)) continue;
 
@@ -43,7 +72,8 @@ export async function parseComponentSchemas(type: string, options: ParseSchemasO
       continue;
     }
 
-    info(`Processing: ${filename}`);
+    const data = fileVersions.get(filename);
+    info(`Processing: ${filename} v${data?.version}`);
     await writeFile(`./src/bp/${type}_components/${filename}.ts`, `// auto generated\n${ts}`, {
       parser: "typescript",
     });
