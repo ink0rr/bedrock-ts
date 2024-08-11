c8a811dd550c3f84b0a3e23938dbd076a6c958a5
Svdex <renixiel@gmail.com>
2024-08-11 23:19:33 +0700

    feat: Add Attachable export in index.ts

--- src/rp/index.ts
+++ src/rp/index.ts
@@ -1,5 +1,6 @@
 export * from "./animation_controllers.js";
 export * from "./animations.js";
+export * from "./attachable.js";
 export * from "./entity.js";
 export * from "./item_texture.js";
 export * from "./particle.js";
