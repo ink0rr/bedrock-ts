68e24f63862abcadbd1d805c152464c3fde0155b
ink0rr <tomflynnbeltsazar@gmail.com>
2024-08-12 00:21:51 +0700

    Run formatter

--- src/rp/index.ts
+++ src/rp/index.ts
@@ -4,5 +4,5 @@ export * from "./attachable.js";
 export * from "./entity.js";
 export * from "./item_texture.js";
 export * from "./particle.js";
-export * from "./sounds.js";
 export * from "./sound_definition.js";
+export * from "./sounds.js";
