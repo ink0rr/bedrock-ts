5c972962fcd0f61ea72c11d4509f2bbc2d737cf5
Svdex <renixiel@gmail.com>
2024-12-04 13:45:58 +0700

    fix: change cannot be attacked to never

--- src/bp/entity_components/cannot_be_attacked.ts
+++ src/bp/entity_components/cannot_be_attacked.ts
@@ -2,4 +2,4 @@
 /**
  * When set, blocks entities from attacking the owner entity unless they have the 'minecraft:ignore_cannot_be_attacked' component.
  */
-export type EntityCannotBeAttackedComponent = {};
+export type EntityCannotBeAttackedComponent = Record<string, never>;
