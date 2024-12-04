1814a0e381d3c137a9799afa89ce9ca148ea9f9c
Svdex <renixiel@gmail.com>
2024-12-04 13:45:27 +0700

    fix: add liquid detection type

--- src/bp/block_components/liquid_detection.ts
+++ src/bp/block_components/liquid_detection.ts
@@ -3,5 +3,24 @@
  * The definitions for how a block behaves when detecting liquid. Only one rule definition is allowed per liquid type - if multiple are specified, the first will be used and the rest will be ignored.
  */
 export type BlockLiquidDetectionComponent = {
-  detection_rules?: Array<any>;
+  detection_rules?: Array<{
+    /**
+     * Whether this block can contain the liquid. For example, if the liquid type is `water`, this means the block can be waterlogged.
+     */
+    can_contain_liquid?: boolean;
+    /**
+     * The type of liquid this detection rule is for.
+     */
+    liquid_type?: "water";
+    /**
+     * How the block reacts to flowing water.
+     */
+    on_liquid_touches?: "blocking" | "broken" | "no_reaction";
+    /**
+     * When a block contains a liquid, controls the directions in which the liquid can't flow out from the block. Also controls the directions in which a block can stop liquid flowing into it if `no_reaction` is set for the `on_liquid_touches` field.
+     */
+    stops_liquid_flowing_from_direction?: Array<
+      "up" | "down" | "north" | "south" | "east" | "west"
+    >;
+  }>;
 };
